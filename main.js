dd399f88434f7aaa90b7162e651f1730dac2d839
manuelaelosantos <163124866+manuelaelosantos@users.noreply.github.com>
2024-10-21 11:33:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,4 +5,30 @@ const baixoOuAlto = document.querySelector('.baixoOuAlto');
 const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
 let contagemPalpites = 1;
-let botaoReiniciar;
+let botaoReiniciar
+
+function verificarPalpite() {
+  const palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPalpites === 1) {
+    palpites.textContent = "Palpites anteriores: ";
+  }
+
+palpites.textContent += palpiteUsuario +n " ";
+
+if (palpiteUsuario === numeroAleatorio) {
+  ultimoResultado.textContent = "Parabéns! Você acertou!";
+  ultimoResultado.style.backgroundColor = "green";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else if (contagemPalpites === 10) {
+  ultimoResultado.textContent = "FIM DE JOGO !!!";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else { 
+ultimoResultado.textContent = "Errado";
+ultimoResultado.style.backgroundColor = "red";
+if (palpiteUsuario < numeroAleatorio) {
+  baixoOuAlto.textContent = "O último palpite foi muito baixo":
+} else if (palpiteUsuario > numeroAleatorio) {
+  baixoOuAlto.textContent = "O último palpite foi muito alto;
+}
